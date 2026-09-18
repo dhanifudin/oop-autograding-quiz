18dd53259c719cc76826b995175bb810f56d1820
Dian Hanifudin Subhi <dhanifudin@gmail.com>
2026-09-18 08:10:46 +0700

    add field and constructor

--- src/main/java/id/ac/polinema/oop/Student.java
+++ src/main/java/id/ac/polinema/oop/Student.java
@@ -10,6 +10,9 @@ package id.ac.polinema.oop;
 public class Student {
 
     // TODO: add fields: studentId (String), name (String), gpa (double)
+    private String studentId;
+    private String name;
+    private double gpa;
 
     /**
      * Student constructor.
@@ -19,8 +22,9 @@ public class Student {
      * @param gpa       Grade Point Average (0.0 - 4.0)
      */
     public Student(String studentId, String name, double gpa) {
-        // TODO: store parameters into fields
-        throw new UnsupportedOperationException("Not implemented yet");
+      this.studentId = studentId;
+      this.name = name;
+      this.gpa = gpa;
     }
 
     public String getStudentId() {
